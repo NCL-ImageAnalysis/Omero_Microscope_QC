f13ed992c8d59b21f84932d850ff861025685021
James Grimshaw <jameswgrimshaw@gmail.com>
2026-04-16 09:28:42 +0100

    dunno what i did last

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#@String name
+
 from metroloJ_QC.setup import MetroloJDialog, QC_Options
 from metroloJ_QC.resolution import PSFprofiler
 from metroloJ_QC.importer import simpleMetaData;
@@ -6,10 +8,12 @@ from java.lang import Double
 from ij import IJ
 
 Opts = QC_Options()
-Dialog = MetroloJDialog("PSF profiler report generator", Opts) # Needs image open
 
-imp = IJ.getImage()
+print('Hello ' + name)
+# Dialog = MetroloJDialog("PSF profiler report generator", Opts) # Needs image open
 
-creationInfo=simpleMetaData.getOMECreationInfos(Dialog.ip, Dialog.debugMode)
+# imp = IJ.getImage()
 
-profiler = PSFprofiler(imp, Dialog, "test", [Double.NaN, Double.NaN], creationInfo)
+# creationInfo=simpleMetaData.getOMECreationInfos(Dialog.ip, Dialog.debugMode)
+
+# profiler = PSFprofiler(imp, Dialog, "test", [Double.NaN, Double.NaN], creationInfo)
