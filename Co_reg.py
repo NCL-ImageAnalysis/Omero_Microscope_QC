cc4b42b352559b91e832bc0c01d2d5f092a8e95e
James Grimshaw <jameswgrimshaw@gmail.com>
2026-04-20 14:49:59 +0100

    report generation

--- Co_reg.py
+++ Co_reg.py
@@ -1,9 +1,15 @@
 from metroloJ_QC.setup import MetroloJDialog, QC_Options
-from metroloJ_QC.coalignment import coAlignment
+from metroloJ_QC.coalignement import coAlignement
 from metroloJ_QC.importer import simpleMetaData
+from metroloJ_QC.report import coAlignementReport
 from java.lang import Double
 
 from ij import IJ
+
+imp = IJ.getImage()
+OrigImgName = imp.getTitle()
+coords = [Double.NaN, Double.NaN]
+output_folder = r"G:\QC\QC_Coreg\Axioimager1"
 
 Opts = QC_Options()
 
@@ -11,8 +17,13 @@ Dialog = MetroloJDialog("Co-registration report generator", Opts) # Needs image 
 Dialog.beadDetectionThreshold = "Otsu"
 Dialog.centerDetectionMethodIndex = 1
 
-imp = IJ.getImage()
+Dialog.savePdf = True
+Dialog.saveSpreadsheet = True
+Dialog.saveImages = True
 
 creationInfo=simpleMetaData.getOMECreationInfos(Dialog.ip, Dialog.debugMode)
 
-coAlign = coAlignment(imp, Dialog, "test", [Double.NaN, Double.NaN], creationInfo)
+coAlign = coAlignement(imp, Dialog, "test", coords, creationInfo)
+
+coreport = coAlignementReport(imp, Dialog, OrigImgName, coords, creationInfo)
+coreport.saveReport(output_folder, "Test_01", None)
