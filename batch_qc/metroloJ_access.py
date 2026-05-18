d5582fae75cb0fc2b3fb9bc15a73fef8ae80b2e0
James Grimshaw <jameswgrimshaw@gmail.com>
2026-05-18 12:24:37 +0100

    refactored so key value pairs used for metroloj settings

--- batch_qc/metroloJ_access.py
+++ batch_qc/metroloJ_access.py
@@ -1,41 +1,25 @@
 import batch_qc
 
-_METROLOJ_DEFAULTS = {
-	
+_METHODS = {
+	"psf": ("PSF profiler report generator", "PSF-profile"),
+	"drift": ("Stage positioning and drift report generator", "Drift-profile"),
+	"registration": ("Co-registration report generator", "Registration-profile")
 }
 
+_CENTER_DETECTION = {"ellipses": 0, "centroid": 1, "max": 2}
 
+_MICROSCOPE_TYPES = {"WideField": 0, "CLSM": 1, "Spinning Disc Confocal": 2, "Multiphoton": 3}
 
 _ACCEPTED_VALUES = {
 	"method": ["psf", "drift", "registration"],
 	"thresholding_method": ["Legacy", "Li", "Minimum", "Otsu"],
 	"center_dectection_method": ["ellipses", "centroid", "max"],
 	"microscope_type": ["WideField", "CLSM", "Spinning Disc Confocal", "Multiphoton"],
-	"pinhole_size_AU": float,
-	"save_pdf": bool,
-	"save_csv": bool,
-	"save_images": bool,
-	"multiple_beads": bool,
-	"bead_channel": int,
-	"bead_size_um": float,
-	"crop_factor": float,
-	"min_distance_to_top_and_bottom_um": float,
-	"reject_doublets": bool,
-	"bead_prominence": float,
-	"inner_annulus_edge_distance_to_bead_um": float,
-	"annulus_thickness_um": float,
-	"apply_tolerance": bool,
-	"coal_ratio_tolerance": float,
-	"XY_ratio_tolerance": float,
-	"Z_ratio_tolerance": float,
-	"uniformity_tolerance": float,
-	"cent_acc_tolerance": float,
-	"max_gap_length": int,
-	"use_resolution_thresholds": bool,
-
-
-
 }
+
+def check_inputs(key, value):
+	if value not in _ACCEPTED_VALUES[key]:
+		raise ValueError(f"{key} must be one of {_ACCEPTED_VALUES[key]}")
 
 def initialize_MetroloJDialog(method,
 							  image,
@@ -43,31 +27,29 @@ def initialize_MetroloJDialog(method,
 							  center_dectection_method="centroid"):
 	if batch_qc._ij is None:
 		raise RuntimeError("ImageJ has not been initialised. Please call batch_qc.initialise() before use.")
-	if method == "psf":
-		method_string = "PSF profiler report generator"
-		title_string = "PSF-profile"
-	elif method == "drift":
-		method_string = "Stage positioning and drift report generator"
-		title_string = "Drift-profile"
-	elif method == "registration":
-		method_string = "Co-registration report generator"
-		title_string = "Registration-profile"
-	else:
-		raise ValueError("Method must be one of 'psf', 'drift' or 'registration'")
+	
+	check_inputs("method", method)
+	method_string, title_string = _METHODS[method]
 
-	if thresholding_method not in ["Legacy", "Li", "Minimum", "Otsu"]:
-		raise ValueError("Thresholding method must be one of 'Legacy', 'Li', 'Minimum', or 'Otsu'")
+	check_inputs("thresholding_method", thresholding_method)
+	check_inputs("center_dectection_method", center_dectection_method)
 	
-	if center_dectection_method == "ellipses":
-		center_integer = 0
-	elif center_dectection_method == "centroid":
-		center_integer = 1
-	elif center_dectection_method == "max":
-		center_integer = 2
-	else:
-		raise ValueError("Center detection method must be one of 'ellipses', 'centroid', or 'max'")
+	center_integer = _CENTER_DETECTION[center_dectection_method]
+	try:
+		check_inputs("microscope_type", image.key_value_pairs["microscope_type"])
+		microscope_index = _MICROSCOPE_TYPES[image.key_value_pairs["microscope_type"]]
+	except KeyError:
+		raise KeyError("Microscope type not found in image key value pairs. Please ensure that the image has a key 'microscope_type' with value 'WideField', 'CLSM', 'Spinning Disc Confocal' or 'Multiphoton'")
 	
-	image_plus = image.generate_ImagePlus()
+	if microscope_index == 1:  # CLSM
+		try:
+			pinhole_size = float(image.key_value_pairs["pinhole_size_AU"])
+		except KeyError:
+			raise KeyError("Pinhole size not found in image key value pairs. Please ensure that the image has a key 'pinhole_size_AU' with the pinhole size in AU as the value.")
+
+	image_plus = image.image_plus
+	if image_plus is None:
+		image_plus = image.generate_ImagePlus()
 
 	batch_qc._java["WindowManager"].setTempCurrentImage(image_plus)
 	Dialog = batch_qc._java["MetroloJDialog"](method_string, batch_qc._java["QC_Options"]())
@@ -75,36 +57,22 @@ def initialize_MetroloJDialog(method,
 	Dialog.beadDetectionThreshold = thresholding_method
 	Dialog.centerDetectionMethodIndex = center_integer
 
-	Dialog.savePdf = save_pdf
-	Dialog.saveSpreadsheet = save_csv
-	Dialog.saveImages = save_images
-
 	Dialog.NA = image.NA
 	Dialog.refractiveIndex = image.refractive_index
 	Dialog.emWavelengths = [ch.emission_wave for ch in image.channels]
 	Dialog.exWavelengths = [ch.excitation_wave for ch in image.channels]
 
-	try:
-		microscope_type = image.key_value_pairs["microscope_type"]
-		if microscope_type == "WideField":
-			Dialog.microType = 0
-		elif microscope_type == "CLSM":
-			Dialog.microType = 1
-		elif microscope_type == "Spinning Disc Confocal":
-			Dialog.microType = 2
-		elif microscope_type == "Multiphoton":
-			Dialog.microType = 3
-		else:
-			raise ValueError("Microscope type must be one of 'WideField', 'CLSM', 'Spinning Disc Confocal' or 'Multiphoton'")
-	except KeyError:
-		raise KeyError("Microscope type not found in image key value pairs. Please ensure that the image has a key 'microscope_type' with value 'WideField', 'CLSM', 'Spinning Disc Confocal' or 'Multiphoton'")
-	
-	if microscope_type == "CLSM":
-		try:
-			Dialog.pinhole = float(image.key_value_pairs["pinhole_size_AU"])
-		except KeyError:
-			raise KeyError("Pinhole size not found in image key value pairs. Please ensure that the image has a key 'pinhole_size_AU' with the pinhole size in AU as the value.")
-
+	class_fields = [class_obj.getName() for class_obj in Dialog.getClass().getFields()]
+	for dict_key in image.key_value_pairs:
+		if dict_key in class_fields:
+			try:
+				setattr(Dialog, dict_key, image.key_value_pairs[dict_key])
+			except TypeError:
+				try:
+					field_type = type(getattr(Dialog, dict_key))
+					setattr(Dialog, dict_key, field_type(image.key_value_pairs[dict_key]))
+				except ValueError:
+					raise TypeError(f"Error: {dict_key} requires input of the type {type(getattr(Dialog, dict_key))}.")
 	return Dialog
 
 def execute_MetroloJ_process(Dialog, report_dir, report_name):
