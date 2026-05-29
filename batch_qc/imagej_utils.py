1e6fbc24a533dc29611883181fe221879cddc094
James Grimshaw <jameswgrimshaw@gmail.com>
2026-05-29 12:58:48 +0100

    swapped for more up to date functions

--- batch_qc/imagej_utils.py
+++ batch_qc/imagej_utils.py
@@ -2,28 +2,42 @@ import re
 import batch_qc
 
 def analyzeParticles(
-		Binary_Image, 
-		Size_Setting, 
-		Circularity_Setting):
+		Binary_Image,
+		size_min = "0.00",
+		size_max = "Infinity",
+		circ_min = "0.00",
+		circ_max = "1.00",
+		exclude=True,
+		stack=False,
+		pixel=False
+		):
 	"""Runs analyze particles on the binary image, returning the ROI
 
 	Args:
 		Binary_Image (ij.ImagePlus): Segmented binary image
-		Size_Setting (str): Min/Max size settings for analyse particles
-		Circularity_Setting (str): Min/Max circularity settings for analyse particles
+		size_min (str): Min size setting for analyse particles. Defaults to "0.00"
+		size_max (str): Max size setting for analyse particles. Defaults to "Infinity"
+		circ_min (str): Min circularity setting for analyse particles. Defaults to "0.00"
+		circ_max (str): Max circularity setting for analyse particles. Defaults to "1.00"
+		exclude (bool): Whether to exclude particles touching the edges of the image. Defaults to True
+		stack (bool): Whether to analyze the entire Z/T stack. Defaults to False
+		pixel (bool): Whether to use pixel units for measurements instead of scaled units. Defaults to False
 
 	Returns:
 		[PolygonRoi]: Outputted Rois
 	"""	
-
+	
+	if batch_qc._ij is None:
+		raise RuntimeError("ImageJ has not been initialised. Please call batch_qc.initialise() before use.")
+	
 	# Defines analyse particles settings
-	AnalyzeParticlesSettings = (
-		"size=" 
-		+ Size_Setting 
-		+ " circularity=" 
-		+ Circularity_Setting 
-		+ " clear overlay exclude"
-	)
+	AnalyzeParticlesSettings = (f"size={size_min}-{size_max} circularity={circ_min}-{circ_max} overlay")
+	if exclude:
+		AnalyzeParticlesSettings += " exclude"
+	if stack:
+		AnalyzeParticlesSettings += " stack"
+	if pixel:
+		AnalyzeParticlesSettings += " pixel"
 	# Runs the analyze particles command to get ROI. 
 	# Done by adding to the overlay in order to not have ROIManger shown to user
 	batch_qc._java["IJ"].run(Binary_Image, "Analyze Particles...", AnalyzeParticlesSettings)
@@ -35,38 +49,89 @@ def analyzeParticles(
 	batch_qc._java["IJ"].run(Binary_Image, "Remove Overlay", "")
 	return RoiList
 
-
 def getRoiMeasurements(SampleRoi, Image, Measurement_Options):
 	"""Gets the given measurements of the provided Roi for the given image
 
 	Args:
 		SampleRoi (ij.gui.Roi): Roi to be analysed
 		Image (ij.ImagePlus): Image to be analysed
-		Measurement_Options ([str]): ij.Measure.Measurements to be taken
+		Measurement_Options ([str]) or ([ij.measure.Measurements]): Measurements to be taken in the form of either strings of the column headings or ij.measure.Measurements integers
 
 	Returns:
-		[float]: List of measurements in same order as Measurement_Options
+		[float]: Dictionary of measurements with column headings as titles
 	"""	
-	
+
+	if batch_qc._ij is None:
+		raise RuntimeError("ImageJ has not been initialised. Please call batch_qc.initialise() before use.")
+
+	Measurements = batch_qc._java["Measurements"]
+
+	# This dictionary converts Measurement_Options to the corresponding column names in the results table
+	Measurement_Dict = {
+		Measurements.ADD_TO_OVERLAY: [None],
+		Measurements.AREA: ['Area'],
+		Measurements.AREA_FRACTION: ['%Area'],
+		Measurements.CENTER_OF_MASS: ['XM', 'YM'],
+		Measurements.CENTROID: ['X', 'Y'],
+		Measurements.CIRCULARITY: ['Circ.', 'AR', 'Round', 'Solidity'],
+		Measurements.ELLIPSE: ['Major', 'Minor', 'Angle'],
+		Measurements.FERET: ['Feret', 'FeretX', 'FeretY', 'FeretAngle', 'MinFeret'],
+		Measurements.INTEGRATED_DENSITY: ['IntDen'],
+		Measurements.INVERT_Y: [None],
+		Measurements.KURTOSIS: ['Kurt'],
+		Measurements.LABELS: ['Label'],
+		Measurements.LIMIT: [None],
+		Measurements.MAX_STANDARDS: [None],
+		Measurements.MEAN: ['Mean'],
+		Measurements.MEDIAN: ['Median'],
+		Measurements.MIN_MAX: ['Min', 'Max'],
+		Measurements.MODE: ['Mode'],
+		Measurements.NaN_EMPTY_CELLS: [None],
+		Measurements.PERIMETER: ['Perim.'],
+		Measurements.RECT: ['ROI_X', 'ROI_Y', 'ROI_Width', 'ROI_Height'],
+		Measurements.SCIENTIFIC_NOTATION: [None],
+		Measurements.SHAPE_DESCRIPTORS: ['Circ.', 'AR', 'Round', 'Solidity'],
+		Measurements.SKEWNESS: ['Skew'],
+		Measurements.SLICE: [None],
+		Measurements.STACK_POSITION: [None],
+		Measurements.STD_DEV: ['StdDev']
+	}
+
 	# Initialises a new empty results table
 	RTable = batch_qc._java["ResultsTable"]()
 	# Initialises an Analyzer object using 
 	# the image and the empty results table
-	An = batch_qc._java["Analyzer"](Image, RTable)
+	try:
+		# If input list is of ij.measure.Measurements will use those measurements for the analyzer
+		Measurement_int = sum(Measurement_Options)
+		An = batch_qc._java["Analyzer"](Image, Measurement_int, RTable)
+	except TypeError:
+		# Otherwise will just use global measurement options
+		Measurement_int = None
+		An = batch_qc._java["Analyzer"](Image, RTable)
 	# Selects the roi on the image
 	Image.setRoi(SampleRoi)
 	# Takes the measurements
 	An.measure()
-	# Takes the desired results from 
-	# the results table and adds to a list
-	OutputList = []
-	for Option in Measurement_Options:
-		OutputList.append(RTable.getValue(Option, 0))
+	# If the measurements were not specified
+	# will use input column headings
+	if Measurement_int == None:
+		Output_List = Measurement_Options
+	# Otherwise will get measurement options from dictionary
+	else:
+		Output_List = []
+		for Option in Measurement_Options:
+			Output_List += Measurement_Dict[Option]
+	# Takes the desired results from the results table and adds to the dictionary
+	OutputDict = {}
+	for Option in Output_List:
+		if Option != None:
+			OutputDict[Option] = RTable.getValue(Option, 0)
 	# Clears the results table
 	RTable.reset()
 	# Clears the roi from the image
 	Image.resetRoi()
-	return OutputList
+	return OutputDict
 
 
 def distanceBetweenPoints(X1, Y1, X2, Y2):
