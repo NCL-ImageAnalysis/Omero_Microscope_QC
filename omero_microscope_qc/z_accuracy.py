57fe5024335e697e372b02ddbaa9d68ef42b8af1
James Grimshaw <jameswgrimshaw@gmail.com>
2026-06-04 14:39:32 +0100

    wrong var going to getProjectedBeads

--- omero_microscope_qc/z_accuracy.py
+++ omero_microscope_qc/z_accuracy.py
@@ -109,7 +109,7 @@ def run_z_accuracy(input_image,
 	ZDepth = Calibration.pixelDepth
 
 	# Runs getProjectedBeads to get a list of ROIs of the ladder
-	RoiList = getProjectedBeads(ZDepth, size_min="10")
+	RoiList = getProjectedBeads(image_plus, size_min="10")
 
 	No_Scale = image_plus.crop()
 	No_Scale.removeScale()
